893670cf89a69a044354e120f6a4f742d42a9825
nestoranaya <nestora@gmail.com>
2021-08-05 21:47:57 -0500

    test_proyectofrontend

---
+++ src/routes/RouterComponent.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+import { Login, Usuario, MenuComponent, VerPartido, RegistrarPartido, GolesPartido } from "../views";
+
+function RouterComponent() {
+    return (
+        <BrowserRouter>
+            <div id="container-notify"></div>
+            <Switch>
+                <Route
+                    path="/login"
+                    render={(props) => <Login {...props} />} />
+                <Route
+                    path="/usuario"
+                    render={(props) => <Usuario {...props} />} />
+                <Route
+                    path="/menu"
+                    render={(props) => <MenuComponent {...props} />} />
+                <Route
+                    path="/verPartido"
+                    render={(props) => <VerPartido {...props} />} />
+                <Route
+                    path="/registrarPartido"
+                    render={(props) => <RegistrarPartido {...props} />} />
+                <Route
+                    path="/golesPartidos"
+                    render={(props) => <GolesPartido {...props} />} />
+
+                <Redirect to="/login" />
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default RouterComponent;
---
+++ src/views/index.js
@@ -0,0 +1,15 @@
+import Login from './login/Login';
+import Usuario from './Usuario/Usuario';
+import MenuComponent from '../components/MenuComponent';
+import VerPartido from './partidos/VerPartido';
+import RegistrarPartido from './partidos/RegistrarPartido';
+import GolesPartido from './partidos/GolesPartido';
+
+export {
+    Login,
+    Usuario,
+    MenuComponent,
+    VerPartido,
+    RegistrarPartido,
+    GolesPartido
+}
